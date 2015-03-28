a9cabc90c2c09276db2322c9131d9854d09125e2
Ethan Madden <crazeh.monkeh@gmail.com>
2015-03-27 21:29:42 -0400

    Modified keyboard layout to include all letters, space, and capslock

--- keyboard.ino
+++ keyboard.ino
@@ -7,10 +7,10 @@ const byte ROWS = 4;
 const byte COLS = 7;
 
 char layout[ROWS][COLS] = {
-  {KEY_1,KEY_2,KEY_3,KEY_4,KEY_5,KEY_6,KEY_7},
   {KEY_Q,KEY_W,KEY_E,KEY_R,KEY_T,KEY_Y,KEY_U},
+  {KEY_I,KEY_O,KEY_P,KEY_K,KEY_L,KEY_Z,KEY_M},
   {KEY_A,KEY_S,KEY_D,KEY_F,KEY_G,KEY_H,KEY_J},
-  {KEY_Z,KEY_X,KEY_C,KEY_V,KEY_B,KEY_N,KEY_M}
+  {KEY_CAPS_LOCK,KEY_X,KEY_C,KEY_V,KEY_B,KEY_N,KEY_SPACE}
 };
 
 byte row[ROWS] = {21,20,19,18};
@@ -46,11 +46,11 @@ void setKey(int keynum, char keypress){
   Keyboard.set_key5(key[4]);
   Keyboard.set_key6(key[5]);
 }
-s
+
 // This method sends the depressed keys and clears the buffer.
 void sendKey(){
   Keyboard.send_now();
-  for(int x = 0; x < 6; x++){ key[x] = 0; }
+  for(int x = 0; x < 8; x++){ key[x] = 0; }
   Keyboard.set_key1(key[0]);
   Keyboard.set_key2(key[1]);
   Keyboard.set_key3(key[2]);
@@ -64,7 +64,7 @@ void loop() {
   for (int c = 0; c < COLS; c++) {
     digitalWrite(col[c], HIGH);
     for (int r = 0; r < ROWS; r++) {
-      if (digitalRead(row[r]) && keycount < 6){
+      if (digitalRead(row[r]) && keycount < 8){
         setKey(keycount, layout[r][c]);
         keycount ++;
       }
